a86df05d91595b3dca0f15432f0d33da9bd3da41
Remel Kabir <remel19@Remels-MacBook-Pro.local>
2015-10-27 12:13:03 -0400

    First programming assignment

---
+++ Assignment_1/pa1.py
@@ -0,0 +1,40 @@
+#Author: Remel Kabir
+#Math 308
+
+import math
+
+
+#Problem No.1
+def quadratic_maximum(b,c):
+    return c-((b**2)/-4)
+
+
+#Problem No.2
+def is_prime(x):
+    n = 2
+    if x == n: 
+        print("True")
+    elif x<n:
+        print("False")
+    else:    
+        while x>n:
+            if x % n == 0:
+                print("Flase")
+                break
+            n = n + 1
+        else:
+            print("True")
+
+
+#Problem No.3
+def newtons_method(f, df, x0, n):
+    if(n<1): 
+        print("The value must be greater than 1 in order to find the root")
+        return
+    counter=0 
+    while(counter != n): 
+        x = x0 - f(x0)/df(x0) 
+        x0= x 
+        counter = counter + 1 
+    return x 
+
